9dab167ee84d4dad099b1ba9d369f80a3765d7bb
A.R <87040192+AntoineRb@users.noreply.github.com>
2022-01-02 14:38:13 +0100

    Add last version of notepad

---
+++ js/modules/notepad-elements.js
@@ -0,0 +1,20 @@
+// Notepad container
+export const notePadContainer = document.querySelector( '.notepad-container' );
+
+// Aside
+export const noteListSection  = notePadContainer.querySelector( '.note-list-block' );
+export const noteList         = noteListSection.querySelector( '.note-list' );
+export const listTopBar       = noteListSection.querySelector( '.list-top-bar' );
+
+export const btnAdd    = listTopBar.querySelector( '.btn-add'  );
+export const btnEdit   = listTopBar.querySelector( '.btn-edit' );
+export const btnDelete = listTopBar.querySelector( '.btn-delete' );
+
+// Article
+export const noteContainer = notePadContainer.querySelector( '.note-container' );
+export const title         = noteContainer.querySelector('header').querySelector('h1');
+export const userNoteInput = noteContainer.querySelector('.user-note-input');
+
+// Modal create new note & edit
+export const dialogTitle   = noteContainer.querySelector('.modal-choose-title');
+export const dialogForm    = dialogTitle.querySelector('form');
